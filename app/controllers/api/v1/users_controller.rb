085798c5a3e9d8d2f22bbe89d997aedf076c1de0
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-10-26 22:44:59 -0400

    accepting form data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_host_for_local_storage
+  wrap_parameters format: [:json, :xml, :url_encoded_form, :multipart_form]
 
   def index
     @users = User.with_attached_avatar
@@ -6,38 +8,72 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-  end
+    puts params
 
-  def show
-    @user = User.find(params[:id])
-    render json: @user, status: :ok
-  end
-
-  def update
     @user = User.find(params[:id])
 
     if params[:avatar]
 
       puts "hi"
       @decoded_file = Base64.decode64(params[:avatar][:data_url])
-        @filename = params[:avatar][:title]
-        @tmp_file = Tempfile.new(@filename)
-        @tmp_file.binmode
-        @tmp_file.write @decoded_file
-        @tmp_file.rewind()
+      @filename = params[:avatar][:title]
+      @tmp_file = Tempfile.new(@filename)
+      @tmp_file.binmode
+      @tmp_file.write @decoded_file
+      @tmp_file.rewind()
 
 
-        @user.avatar.attach(io: @tmp_file, filename: @filename)
-        @user.save
+      @user.avatar.attach(io: @tmp_file, filename: @filename)
+      @user.save
+      @tmp_file.unlink
+
+
+
+    end
+    @user.update(location: params[])
+
+    render json: @user, status: :ok
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # @avatar_url = rails_blob_path(@user.avatar)
+    render json: @user, status: :ok
+  end
+
+  def update
+    puts params
+
+    @user = User.find(params[:id])
+
+    if params[:avatar]
+
+      puts "hi"
+      @decoded_file = Base64.decode64(params[:avatar][:data_url])
+      @filename = params[:avatar][:title]
+      @tmp_file = Tempfile.new(@filename)
+      @tmp_file.binmode
+      @tmp_file.write @decoded_file
+      @tmp_file.rewind()
+
+
+      @user.avatar.attach(io: @tmp_file, filename: @filename)
+      @user.save
+      @tmp_file.unlink
 
 
 
     end
     @user.update(user_params)
-    render json: @user.with_attached_avatar, status: :ok
+
+    render json: @user, status: :ok
   end
 
   private
+
+  def set_host_for_local_storage
+    ActiveStorage::Current.host = request.base_url
+  end
 
   def user_params
     params.require(:user).permit(:username, :location, :avatar)
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,24 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :username, :location, :avatar_url
+  attributes :id, :username, :location  
+   # :avatar_path, :avatar_image_url
 
-  def avatar_url
-    rails_blob_path(object.avatar) if object.avatar.attachment
-  end
+  # def avatar_image_url
+  #   Rails.application.routes.url_helpers.url_for(object.avatar) if object.avatar.attached?
+  # end
+  #
+  # def avatar_path
+  #   if object.avatar.attached?
+  #     variant = object.avatar.variant(resize: "100x100")
+  #     Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true)
+  #     # return rails_representation_url(variant, only_path: true)
+  #     # return rails_blob_url(object.avatar, only_path: true)
+  #   end
+  # end
+  #
+
+
 
 
 end
