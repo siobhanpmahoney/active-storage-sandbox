d530beab7fb940eeb45ce95d7afefc3432d21e65
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-10-27 21:37:26 -0400

    SUCCESS uploading images via form and rendering working url

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Api::V1::UsersController < ApplicationController
   before_action :set_host_for_local_storage
   wrap_parameters format: [:json, :xml, :url_encoded_form, :multipart_form]
@@ -9,70 +11,106 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     puts params
-
-    @user = User.find(params[:id])
-
-    if params[:avatar]
-
-      puts "hi"
-      @decoded_file = Base64.decode64(params[:avatar][:data_url])
-      @filename = params[:avatar][:title]
-      @tmp_file = Tempfile.new(@filename)
-      @tmp_file.binmode
-      @tmp_file.write @decoded_file
-      @tmp_file.rewind()
-
-
-      @user.avatar.attach(io: @tmp_file, filename: @filename)
-      @user.save
-      @tmp_file.unlink
-
-
-
-    end
-    @user.update(location: params[])
-
-    render json: @user, status: :ok
   end
 
   def show
     @user = User.find(params[:id])
     # @avatar_url = rails_blob_path(@user.avatar)
+    # render json: {user: @user, avatar: @user.avatar, avatar_url: rails_blob_url(@user.avatar)}, serializer: nil, status: :ok
     render json: @user, status: :ok
   end
 
   def update
     puts params
 
+
     @user = User.find(params[:id])
 
     if params[:avatar]
+      # puts "hi"
+      # puts "\n"
+      # puts "\n"
+      # puts "\n"
+      # puts "\n"
+      # puts "params[:avatar]"
+      # puts "\n"
+      # puts "\n"
+      # puts params[:avatar]
+      # puts "\n"
+      # puts "\n"
+      # puts "params[:avatar].io"
+      # puts "\n"
+      # puts "\n"
+      # puts params[:avatar].to_io
+      # puts "\n"
+      # puts "\n"
+      # puts "\n"
+      # puts "\n"
+      # puts "File.new(params[:avatar].to_io)"
+      # puts File.new(params[:avatar].to_io)
+      # puts "\n"
 
-      puts "hi"
-      @decoded_file = Base64.decode64(params[:avatar][:data_url])
-      @filename = params[:avatar][:title]
-      @tmp_file = Tempfile.new(@filename)
-      @tmp_file.binmode
-      @tmp_file.write @decoded_file
-      @tmp_file.rewind()
+
+      @user.avatar.attach(params[:avatar])
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "url_for(@user.avatar)"
+      puts url_for(@user.avatar)
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "\n"
+      puts "rails_blob_path(@user.avatar, disposition: attachment)"
+      puts rails_blob_url(@user.avatar)
+      puts "\n"
+      puts "\n"
+      puts "\n"
+
+      # @decoded_file = Base64.decode64(params[:avatar][:data_url])
+      # @filename = params[:avatar_title]
+      # @tmp_file = Tempfile.new(@filename)
+      # @tmp_file.binmode
+      # @tmp_file.write @decoded_file
+      # @tmp_file.rewind()
+      #
 
 
-      @user.avatar.attach(io: @tmp_file, filename: @filename)
-      @user.save
-      @tmp_file.unlink
+      # params[:avatar].tempfile
+      # .avatar.attach(params[:avatar])
+      # @file = params[:avatar]
+      #
+      #
+
+
+
+
+      # @user.avatar.attach(io: File.open(@file), filename: params[:avatar][:title], content_type: params[:avatar][:type], identify: false)
+
+
+      # @user.save
+      # @tmp_file.unlink
+      # @user.save!
+
 
 
 
     end
-    @user.update(user_params)
+    @user.update(username: params[:username], location: params[:location])
 
-    render json: @user, status: :ok
+    render json: {user: @user, avatar: @user.avatar}, status: :ok
   end
 
   private
 
   def set_host_for_local_storage
-    ActiveStorage::Current.host = request.base_url
+    ActiveStorage::Current.host = request.base_url if Rails.application.config.active_storage.service == :local
   end
 
   def user_params
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,25 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :username, :location  
-   # :avatar_path, :avatar_image_url
+  attributes :id, :username, :location, :avatar, :avatar_blob_path
 
+  def avatar
+     Rails.application.routes.url_helpers.url_for(object.avatar) if object.avatar.attachment
+  end
+
+
+
+  def avatar_blob_path
+     Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true) if object.avatar.attachment
+  end
+
+  # :avatar, :avatar_path, :avatar_image_url
+  #
+  # def avatar
+  #   Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
+  # end
+  #
+  #
   # def avatar_image_url
   #   Rails.application.routes.url_helpers.url_for(object.avatar) if object.avatar.attached?
   # end
@@ -16,9 +32,12 @@ class UserSerializer < ActiveModel::Serializer
   #     # return rails_blob_url(object.avatar, only_path: true)
   #   end
   # end
-  #
+
 
 
 
 
 end
+# rails_blob_url(@user.avatar)
+# Rails.application.routes.url_helpers.rails_blob_path(@user.avatar.attachment)
+#  Rails.application.routes.url_helpers.url_for(@user.avatar.attachment)
